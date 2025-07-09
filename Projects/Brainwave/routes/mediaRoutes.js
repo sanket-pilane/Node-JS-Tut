3c2159072a02cdd546c43170c706690fcdc15147
sanket-pilane <lalitpilane7@gmail.com>
2025-07-09 13:17:50 +0530

    refactor: Media routes changed due to apikey shortage

--- Projects/Brainwave/routes/mediaRoutes.js
+++ Projects/Brainwave/routes/mediaRoutes.js
@@ -10,7 +10,16 @@ router.get("/", (req, res) => {
   res.json({ message: "Home route" });
 });
 
-router.post("/video", videoController);
-router.post("/image", imageController);
-router.post("/music", musicController);
+router.post("/video", (req, res) => {
+  res.json({ message: "video route" });
+});
+router.post("/image", (req, res) => {
+  res.json({ message: "image route" });
+});
+router.post("/music", (req, res) => {
+  res.json({ message: "music route" });
+});
+// router.post("/video", videoController);
+// router.post("/image", imageController);
+// router.post("/music", musicController);
 module.exports = router;
