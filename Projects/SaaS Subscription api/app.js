941f5c28fd810589c06fadb46e76f615052db5c7
sanket-pilane <lalitpilane7@gmail.com>
2025-07-11 17:28:48 +0530

    A SaaS app projec init and setup

---
+++ Projects/SaaS Subscription api/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("SaaS Backend is live");
+});
+module.exports = app;
