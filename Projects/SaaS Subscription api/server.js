ca4402b67ff95afb2fdaee718dc8011d263615e6
sanket-pilane <lalitpilane7@gmail.com>
2025-07-14 10:07:42 +0530

    fix: minor changes

--- Projects/SaaS Subscription api/server.js
+++ Projects/SaaS Subscription api/server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const db = require("./config/db");
 require("dotenv").config();
-const userRoutes = require("./routes/auth.route");
 const PORT = process.env.PORT || 3000;
 
 db();
@@ -9,5 +8,5 @@ db();
 app.use("/api/user", require("./routes/user.route"));
 app.use("/api/admin", require("./routes/admin.route"));
 app.use("/api/premium", require("./routes/premium.route"));
-app.use("/api/auth", userRoutes);
+app.use("/api/auth", require("./routes/auth.route"));
 app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`));
